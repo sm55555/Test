7cf8b40b0cbbf1edb247d68593fb000bb27983e8
ohsangmin <rhksdma93@gmail.co>
2020-05-04 15:24:07 +0900

    test

--- src/main/java/com/ohsangmin/test/springboot/service/PostsService.java
+++ src/main/java/com/ohsangmin/test/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.ohsangmin.test.springboot.service;
 
 import com.ohsangmin.test.springboot.domain.posts.Posts;
 import com.ohsangmin.test.springboot.domain.posts.PostsRepository;
+import com.ohsangmin.test.springboot.web.dto.PostsListResponseDto;
 import com.ohsangmin.test.springboot.web.dto.PostsResponseDto;
 import com.ohsangmin.test.springboot.web.dto.PostsSaveRequestDto;
 import com.ohsangmin.test.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +33,27 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+
 }
